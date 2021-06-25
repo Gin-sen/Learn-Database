b7d9508884d3feed2e873d53a543e5770a904ed5
places_m <places_m@etna-alternance.net>
2021-06-25 22:33:11 +0200

    fix volumes + connection

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM mongo:4.0.22
-
+EXPOSE 27017
 COPY ./script_db/ /docker-entrypoint-initdb.d/
-
+COPY ./fixtures /data/fixtures/
 
 # RUN apt-get update \
 #     && apt-get install -y git build-essential
+
+CMD ["mongod"]
--- script_db/1-data.sh
+++ script_db/1-data.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Import from fixtures
 
-mongoimport --db learn_db --collection types --file /data/fixtures/types.json && \
-mongoimport --db learn_db --collection questions --file /data/fixtures/questions.json && \
-mongoimport --db learn_db --collection users --file /data/fixtures/users.json
+mongoimport --db learn_db --collection types --file /data/fixtures/types.json && mongoimport --db learn_db --collection questions --file /data/fixtures/questions.json && mongoimport --db learn_db --collection users --file /data/fixtures/users.json
